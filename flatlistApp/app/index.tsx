ac1c8af7b3873d65a5bff64efe65e1e0bf4b4ab6
bouldie1 <145374673+bouldie1@users.noreply.github.com>
2025-01-29 11:02:47 -0600

    Basics of FlatList added

--- flatlistApp/app/index.tsx
+++ flatlistApp/app/index.tsx
@@ -7,8 +7,31 @@ import {
 } from "react-native";
 import colors from "../styles/colors";
 import defaultStyles from "../styles/defaultStyles";
+import { useState } from "react";
 
 export default function Index() {
+
+  type dataType = {
+    id: string; // Unique identifier for each element in list
+    title: string // What I display
+  }
+
+  const DATA: dataType[] = [
+    { id: '1', title: 'First' },
+    { id: '2', title: 'Second' },
+    { id: '3', title: 'Third' },
+    { id: '4', title: 'Fourth' }
+  ];
+
+  const [selectedId, setSelectedId] = useState<string>('1')
+
+  // Create a simply function to call when an items is selected
+  // Pass a param of the item that was clicked on
+  const selectedList = (item: dataType) => {
+    console.log('Clicked on ' + item.title)
+    setSelectedId(item.id)
+  }
+
   return (
     <View style={defaultStyles.container}>
       <View style={defaultStyles.titleContainer}>
@@ -16,7 +39,31 @@ export default function Index() {
       </View>
       <View style={[defaultStyles.textContainer, { flex: 1 }]}>
         <View style={styles.flatlist}>
-          <Text>This is where our list will go</Text>
+          <FlatList 
+            data = {DATA}
+            keyExtractor = {(item: dataType) => item.id}
+            renderItem = {({ item }) => (
+              <TouchableOpacity onPress={() => selectedList(item)}>
+                <View style={[styles.titleContainer,
+                  {backgroundColor: 
+                    item.id === selectedId 
+                      ? colors.primary 
+                      : colors.secondary
+                    }
+                  ]}
+                >
+                  <Text style={[styles.titleText,
+                    {
+                      color:
+                      item.id === selectedId 
+                      ? colors.text.light 
+                      : colors.text.dark
+                    }
+                  ]}>{item.title}</Text>
+                </View>
+              </TouchableOpacity>
+            )}
+          />
         </View>
       </View>
     </View>
